64c94b6d6d0236bc9c53359065fe7dc3ab4b3be9
William Ngo <will@mrngoitall.net>
2013-08-28 18:43:10 -0700

    Passed all tests

--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,7 +1,15 @@
-exports.readUrls = function(filePath, cb){
+var fs = require('fs');
 
+exports.readUrls = function(filePath, cb){
+  var sites = fs.readFileSync(filePath, 'utf8').split("\n");
+  cb(sites);
 };
 
 exports.downloadUrls = function(urls){
   // fixme
+  if (typeof urls === 'array') {
+    for (var i = 0; i < urls.length; i++) {
+      
+    }
+  }
 };
