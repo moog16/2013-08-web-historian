08e8312af7ebced7b538a9f8522fb4effa59b0c7
William Ngo <will@mrngoitall.net>
2013-08-28 19:25:47 -0700

    got htmlfetcher working

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,10 @@
 // eventually, you'll have some code here that uses the tested helpers 
 // to actually download the urls you want to download.
+var urlHelper = require('./lib/html-fetcher-helpers');
+var fs = require('fs');
+var mainRootArray = __dirname.split("/");
+var mainRoot = mainRootArray.slice(0,mainRootArray.length-1).join("/");
+
+urlHelper.readUrls(mainRoot + '/data/sites.txt', function(sites){
+  urlHelper.downloadUrls(sites);
+});
--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,15 +1,26 @@
 var fs = require('fs');
+var http = require('http-get');
+var mainRootArray = __dirname.split("/");
+var mainRoot = mainRootArray.slice(0,mainRootArray.length-2).join("/");
 
 exports.readUrls = function(filePath, cb){
+  console.log('readURLS ISISIISI is running');
   var sites = fs.readFileSync(filePath, 'utf8').split("\n");
   cb(sites);
 };
 
 exports.downloadUrls = function(urls){
-  // fixme
-  if (typeof urls === 'array') {
-    for (var i = 0; i < urls.length; i++) {
-      
+  if (Array.isArray(urls)) {
+    for (var i = 0; i < urls.length-1; i++) {
+      console.log('this is running!!!');
+      http.get(urls[i], mainRoot + '/data/sites/'+urls[i], function(err, result) {
+        if(err) {
+          throw new Error('Unable to download site: ' + err);
+          return;
+        } 
+        console.log('File downloaded at: ' + result.file);
+        
+      });
     }
   }
 };
